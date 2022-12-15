6b66909ae378448cfc8a9560afb2cf6e0d01dc2a
ilgurgenefe <ilgurgenefe@sabanciuniv.edu>
2022-12-15 05:38:50 +0300

    typo fixed

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.use(fileupload({useTempFiles: true}));
+app.use(fileUpload({useTempFiles: true}));
 //routes
 app.use('/', pageRoute);
 app.use('/news', newsRoute);
